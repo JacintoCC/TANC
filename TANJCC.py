52206fac70786104f5a568e3e76093efdaa48d7d
JacintoCC <jacintocc0@gmail.com>
2016-03-08 02:50:50 +0100

    Práctica 5 antes de ser enunciada

--- TANJCC.py
+++ TANJCC.py
@@ -10,6 +10,7 @@ import os, sys
 
 from sympy import Symbol
 from sympy import gcdex
+from random import randint
 
 hexa_to_dec_dict = {str(x): x for x in range(0,10)}
 hexa_to_dec_dict.update({'a': 10, 'b':11, 'c':12, 'd':13, 'e':14, 'f':15})
@@ -148,3 +149,95 @@ def deco_rsa(lista,e,p,q):
 		decoded_list.append(dectohex_rec(decoded_number))
 
 	return hexalist_to_str(decoded_list)
+
+def psp(n):
+	b = randint(2, n-1)
+	d = gcdex(b, n)[2]
+	if( d != 1):
+		return [b, False]
+	else:
+		v = pow(b, n-1, n)
+		return [b, v == 1]
+
+print( psp(561) )
+
+def psp_k(n, k):
+	i = 1
+	psp_n = psp(n)
+	not_prime = psp_n[1]
+	while( i in range(1,k) and not_prime):
+		psp_n = psp(n)
+		not_prime = psp_n[1]
+		i = i+1
+
+	return psp_n
+
+print( psp_k(561,10) )
+
+def legendre_symbol(d, p):
+	if( d % p == 0 ):
+		return 0
+	else:
+		if( d > p ):
+			return legendre_symbol(d%p, p)
+		else
+			return (-1)^((p-1)*(q-1)/4)*legendre_symbol(p%d, d)
+
+def epsp(n):
+	if( n%2 == 0):
+		return [2, False]
+	else:
+		b = randint(3, n-1)
+		d = gcdex(b, n)[2]
+		if( d != 1):
+			return [b, False]
+		else:
+			return [b, pow(b, (n-1)/2, n) == legendre_symbol(b,n)%n]
+
+
+def epsp_k(n, k):
+	i = 1
+	epsp_n = epsp(n)
+	not_prime = epsp_n[1]
+	while( i in range(1,k) and not_prime):
+		epsp_n = epsp(n)
+		not_prime = epsp_n[1]
+		i = i+1
+
+	return epsp_n
+
+def fact_k(n, k):
+	s = 0
+	while( n%k == 0 ):
+		n = k/n
+		s = s + 1
+
+	return [n, s]
+
+def fpsp(n):
+	if( n%2 == 0):
+		return [2, False]
+	else:
+		[s,t] = fact_k(n-1, 2)
+		b = randint(2, n-1)
+		d = gcdex(b, n)[2]
+		if( d != 1):
+			return [b, False]
+		else if( pow(b,t,n) == 1 or pow(b,t,n)==-1):
+			return [b, True]
+		else:
+			for( i in range(1,s-1)):
+				if( pow(b^t,2^i,n) == -1):
+					return [b, True]
+			return [b, False]
+
+def fpsp_k(n, k):
+	i = 1
+	fpsp_n = fpsp(n)
+	not_prime = fpsp_n[1]
+	while( i in range(1,k) and not_prime):
+		fpsp_n = fpsp(n)
+		not_prime = fpsp_n[1]
+		i = i+1
+
+	return fpsp_n		
